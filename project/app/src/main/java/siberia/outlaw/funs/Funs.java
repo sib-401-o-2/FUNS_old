ced1c63772a0a1274d75a3f7f0ece5173ff71361
Viktor Filinkov <bruteforce@sigil.tk>
2015-12-10 20:53:33 +0600

    Now works with odd and even files too

--- project/app/src/main/java/siberia/outlaw/funs/Funs.java
+++ project/app/src/main/java/siberia/outlaw/funs/Funs.java
@@ -1,11 +1,16 @@
 package siberia.outlaw.funs;
 
+import android.support.v4.content.res.TypedArrayUtils;
+
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static siberia.outlaw.funs.FunsScheduleParser.*;
 
 /**
  * Created by asmodeus on 14.11.15.
@@ -14,6 +19,8 @@ public class Funs {
     private static final String funsDirName = "/storage/emulated/0" + "/.FUNS/";
     private static final String timeFilename = "time.json";
     private static final String scheduleFilename = "schedule.json";
+    private static final String scheduleOddFilename = "scheduleOdd.json";
+    private static final String scheduleEvenFilename = "scheduleEven.json";
 
     private static FunsTime time;
     private static FunsSchedule schedule;
@@ -21,16 +28,34 @@ public class Funs {
 
     private Funs() {
         time = FunsTimeParser.getTime(funsDirName + timeFilename);
+
+        FunsSchedule.Day[] days;
+        FunsSchedule.Course[] courses;
+
+        File file = new File(funsDirName + scheduleFilename);
+        if(file.exists()) {
+            days = getScheduleDays(funsDirName + scheduleFilename);
+            courses = getCourses(funsDirName + scheduleFilename);
+            System.out.println("неждан!");
+        } else {
+            FunsSchedule.Day[] odd = getScheduleDays(funsDirName + scheduleOddFilename);
+            FunsSchedule.Day[] even = getScheduleDays(funsDirName + scheduleEvenFilename);
+            days = new FunsSchedule.Day[14];
+            System.arraycopy(odd, 0, days, 0, odd.length);
+            System.arraycopy(even, 0, days, odd.length, odd.length);
+            courses = getCourses(funsDirName + scheduleOddFilename);
+            System.out.println("ждан!" + days.length);
+        }
         schedule = new FunsSchedule(
-                FunsScheduleParser.getCourses(funsDirName + scheduleFilename),
-                FunsScheduleParser.getScheduleDays(funsDirName + scheduleFilename)
+                courses,
+                days
         );
 
         subjects = new ArrayList<>(14);
         for (int i = 1; i<=7; i++) {
             ArrayList<Subject> records = new ArrayList<Subject>();
             for (int si = 0; si < 10; si++) {
-                Subject record = Funs.getInstance().getSchedule().getSubject(i, si, true); // odd
+                Subject record = getSchedule().getSubject(i, si, true); // odd
                 if (record != null) {
                     records.add(record);
                     System.out.println("gening ODD: day " + i + " " + record.getName());
